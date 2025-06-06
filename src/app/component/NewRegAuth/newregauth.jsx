3365e0f5a09fb6a230f833655fbb6375a013331c
kaito0804 <kaitonbsk.0804@gmail.com>
2025-06-06 17:47:56 +0900

    ⚡️ 承認用UI追加

--- src/app/component/NewRegAuth/newregauth.jsx
+++ src/app/component/NewRegAuth/newregauth.jsx
@@ -2,29 +2,29 @@
 import { supabase } from "@/app/utils/supabase/supabaseClient";
 
 export default function AuthButtons() {
-  const signInWithGoogle = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: 'google',
-    });
-  };
+	const signInWithGoogle = async () => {
+		await supabase.auth.signInWithOAuth({
+		provider: 'google',
+		});
+	};
 
-  const signInWithApple = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: 'apple',
-    });
-  };
+	const signInWithApple = async () => {
+		await supabase.auth.signInWithOAuth({
+		provider: 'apple',
+		});
+	};
 
-  const signInWithEmail = async () => {
-    const { data, error } = await supabase.auth.signInWithOtp({
-      email: 'your@example.com', // ← 入力フォームから取得するように
-    });
-  };
+	const signInWithEmail = async () => {
+		const { data, error } = await supabase.auth.signInWithOtp({
+			email: 'your@example.com', // ← 入力フォームから取得するように
+		});
+	};
 
-  return (
-    <div className="flex flex-col gap-2">
-      <button onClick={signInWithGoogle}>Googleでログイン</button>
-      <button onClick={signInWithApple}>Appleでログイン</button>
-      <button onClick={signInWithEmail}>メールでログインリンクを送る</button>
-    </div>
-  );
+	return (
+		<div className="absolute bottom-[0] flex flex-col justify-center items-center gap-[30px] w-[100%] py-[35px] bg-[rgba(51,51,51,0.8)] rounded-t-[20px]">
+			<div onClick={signInWithGoogle} className="w-[300px] py-[12px] px-[5px] flex flex-col justify-center items-center bg-[#ff9731] text-white rounded-[100px] font-bold">Googleでログイン</div>
+			<div onClick={signInWithApple}  className="w-[300px] py-[12px] px-[5px] flex flex-col justify-center items-center bg-[#ff9731] text-white rounded-[100px] font-bold">Appleでログイン</div>
+			<div onClick={signInWithEmail}  className="w-[300px] py-[12px] px-[5px] flex flex-col justify-center items-center bg-[#ff9731] text-white rounded-[100px] font-bold">メールでログインリンクを送る</div>
+		</div>
+	);
 }
