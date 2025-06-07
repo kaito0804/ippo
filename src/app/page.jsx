ce73d03dd569dc67dfbfca6391901faad6af79d6
kaitonbsk0804 <kaitonbsk.0804@gmail.com>
2025-06-07 12:58:39 +0900

    ⚡️ 承認用UI追加2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,30 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 import AuthButtons from "@/app/component/NewRegAuth/newregauth";
 
 export default function Home() {
-	return (
-		<div className="flex flex-col justify-center items-center">
 
-			<div className="flex flex-col justify-center items-center w-[100%] h-[100vh] bg-[#fffcfa]">
-				<div className="absolute top-[calc(50%-120px)] flex flex-col justify-center items-center text-[#ff7a00]">
-					<p className="font-bold text-[60px]">IPPO</p>
-					<p className="font-bold text-[18px]">散歩コミュニティ</p>
+	const [authBtn, setAuthBtn] = useState('');
+
+	return (
+		<div>
+			<div onClick={() => setAuthBtn('')} className="flex flex-col justify-center items-center">
+
+				<div className="flex flex-col justify-center items-center w-[100%] h-[100vh] bg-[#fffcfa]">
+					<div className="absolute top-[calc(50%-120px)] flex flex-col justify-center items-center text-[#ff7a00]">
+						<p className="font-bold text-[60px]">IPPO</p>
+						<p className="font-bold text-[18px]">散歩コミュニティ</p>
+					</div>
+				</div>	
+				
+				<div onClick={(e) => e.stopPropagation()} className={`${authBtn ? 'bottom-[-100%]' : 'bottom-[80px]'} absolute flex flex-col justify-center items-center gap-[30px] transition-all duration-500`}>
+					<div onClick={() => setAuthBtn('new_reg')} className="w-[300px] py-[12px] px-[5px] flex flex-col justify-center items-center bg-[#ff9731] text-white rounded-[100px] font-bold">新規アカウント作成</div>
+					<div onClick={() => setAuthBtn('login')} className="w-[300px] py-[12px] px-[5px] flex flex-col justify-center items-center bg-[#4363ff] text-white rounded-[100px] font-bold">ログイン</div>
 				</div>
 			</div>	
-			
-			<div className="absolute bottom-[80px] flex flex-col justify-center items-center gap-[30px]">
-				<div className="w-[300px] py-[12px] px-[5px] flex flex-col justify-center items-center bg-[#ff9731] text-white rounded-[100px] font-bold">新規アカウント作成</div>
-				<div className="w-[300px] py-[12px] px-[5px] flex flex-col justify-center items-center bg-[#4363ff] text-white rounded-[100px] font-bold">ログイン</div>
-			</div>
 
-			<AuthButtons />
-
+			<AuthButtons authBtn={authBtn} setAuthBtn={setAuthBtn}/>
 		</div>
 	);
 }
