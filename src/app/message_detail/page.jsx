e4a40467174223712863134c46fd6a499d675cb4
kaito0804 <kaitonbsk.0804@gmail.com>
2025-07-01 16:21:06 +0900

    ⚡️ 修正用

--- src/app/message_detail/page.jsx
+++ src/app/message_detail/page.jsx
@@ -1,5 +1,6 @@
 import MessageDetailClient from '@/app/component/MessageDetailClient/MessageDetailClient';
 
-export default function MessageDetailPage() {
-  return <MessageDetailClient />;
+export default function MessageDetailPage({ searchParams }) {
+ 	return <MessageDetailClient groupId={searchParams.groupId} />;
 }
+
